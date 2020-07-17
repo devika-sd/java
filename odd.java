33f7ce9db8d15d2496b41e9a1cd833ecf5ef3005
padmadevika J <42245907+devika-sd@users.noreply.github.com>
2020-07-17 19:50:25 +0530

    Update odd.java

--- odd.java
+++ odd.java
@@ -1,1 +1,11 @@
 
+public class odd {  
+public static void main(String[] args) {  
+    //Code of Java for loop  
+boolean f=false;
+    for(int i=1;f;i++)
+    {  
+        System.out.println(i);  
+    }  
+}  
+}  
