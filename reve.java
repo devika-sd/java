81b4604126940c3a637dd5a442a8db3b97f9710c
padmadevika J <42245907+devika-sd@users.noreply.github.com>
2020-07-18 20:13:13 +0530

    Update reve.java

--- reve.java
+++ reve.java
@@ -1,1 +1,29 @@
-
+import java.util.*;
+class reve
+{
+	public static void main(String args[])
+	{
+		reve c=new reve();
+		String str=c.fun("Hello master",3);
+		System.out.println(str);
+	}
+	public String fun(String str1,int size)
+	{
+		int i,j,k;
+		j=size;
+		k=0;
+		char[] str=new char[str1.length()];
+		for(i=0;i<str1.length();i++)
+		{
+			j--;
+			str[i]=str1.charAt(j);
+			if(j==k)
+			{
+				k=k+(size);
+				j=k+3;
+			}
+		}
+		String fin=new String(str);
+		return fin;
+	}
+}
