80a287b321ea6ee929ab285a01a844639d523121
padmadevika J <42245907+devika-sd@users.noreply.github.com>
2020-07-20 21:52:38 +0530

    Update Sorting_first_second.java

--- Sorting_first_second.java
+++ Sorting_first_second.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+class Sorting_first_second {
+	public static void main(String args[]) {
+	   Scanner s=new Scanner(System.in);
+	   int n=s.nextInt();
+	   int arr[]=new int[n];
+	   for(int i=0 ; i<n ; i++) {
+	     arr[i]=s.nextInt();
+	   }
+	   Arrays.sort(arr);
+	   for(int i=0;i<n/2;i++) {
+	       System.out.print(arr[i]+" ");
+	    }
+	    for(int j=n-1;j>=n/2;j--) {
+	        System.out.print(arr[j]+" ");
+	     }
+}
+}   
